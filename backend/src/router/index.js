3110db00e692f8a2ca2d0bb6e6a12f667e3b092c
thtocha <max.tatochenko@gmail.com>
2024-01-03 10:18:50 +0200

    Create guest layout & use it in Login. ResetPassword & PequestPassword.

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import Login from "@/views/Login.vue";
 import Dashboard from "@/views/Dashboard.vue";
+import RequestPassword from "@/views/RequestPassword.vue";
+import ResetPassword from "@/views/ResetPassword.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,16 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: Login
+        },
+        {
+            path: '/request-password',
+            name: 'requestPassword',
+            component: RequestPassword
+        },
+        {
+            path: '/reset-password/:token',
+            name: 'resetPassword',
+            component: ResetPassword
         }
     ]
 })
